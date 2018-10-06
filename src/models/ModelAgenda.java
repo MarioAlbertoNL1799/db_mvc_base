1ee46c5596c6fc15153a06c6836859c2e259bffe
MarioAlbertoNL1799 <37378931+MarioAlbertoNL1799@users.noreply.github.com>
2018-10-05 22:14:47 -0500

    0.- Se documenta todos los nuevos metodos. 1.- Se pregunta si se desea eliminar un registro en caso de ser asi se hace lo siguiente:     1.1.- Se crea el metodo borrar el cual toma el ide del contacto actual.     1.2.- Se hace una sentencia de SQL donde se eleimina el registro acorde al id.     1.3.- Se envia un mansaje de actualizacion y se actualiza la tabla.     1.4.- Se reinicia la conexion a la base de datos y se muestra el primer registro

--- src/models/ModelAgenda.java
+++ src/models/ModelAgenda.java
@@ -146,6 +146,12 @@ public class ModelAgenda {
         }
     }
     
+    /**
+     *Método que inserta un nuevo registro que realiza lo siguiente:
+     * 1.- Inserta un nuevo registro tomando los datos de la variable nombre y email
+     * 2.- Realiza una conexion nuevamente con la base de datos
+     * 3.- Muestra los datos del ultimo registro
+     */
     public void Insertar(){
         try{
                 st.executeUpdate("Insert into contactos (nombre,email)"+ "values( '"+nombre+"','"+email+"');");
@@ -156,6 +162,15 @@ public class ModelAgenda {
              JOptionPane.showMessageDialog(null, "Error ModelAgenda 006: " + err.getMessage());
         }
     }
+    
+    /**
+     * Método que realiza lo siguiente:
+     * 1.- Toma y almacena el numero del registro actual.
+     * 2.- Actualiza los valores de nombe y email de dicho registro.
+     * 3.- Envia un mensaje de actualizacion.
+     * 4.- Realiza una nueva conexion a la base de datos.
+     * 5.- Muestra el registro con la actualizacion hecha.
+     */
     public void Modificar(){
         try{
             id = rs.getInt("id_contacto");
@@ -166,6 +181,30 @@ public class ModelAgenda {
             rs.absolute(id);
         } catch(SQLException err){
             JOptionPane.showMessageDialog(null,"ErrorModelAgenda 007: " + err.getMessage());
+        }
+    }
+    
+    /**
+     * Método que permite lo siguiente:
+     * 1.- Toma el valor del contacto actual.
+     * 2.- Elimina el dato de la tabla y envia un mensaje de actualizacion.
+     * 3.- ctualiza la base de datos y se reinicia la conexion con la base de datos.
+     * 4.- Mostrar los datos de el contacto anterior.
+     */
+    public void Borrrar(){
+        JOptionPane.showConfirmDialog(null, "¿Desea eliminar el registro?");
+        if(JOptionPane.OK_OPTION == 0){
+            try{
+                id = rs.getInt("id_contacto");
+                st.executeUpdate("delete from contactos where id_contacto =" + id + "; ");
+                JOptionPane.showMessageDialog(null, "Agenda actualizada, el registro ha sido eliminado");
+                st.executeQuery("select * from contactos");
+                conectarDB();
+                rs.first();
+            }
+            catch(SQLException err){
+               JOptionPane.showMessageDialog(null, "Error ModelAgenda 008:" + err.getMessage());
+            }
         }
     }
 }
